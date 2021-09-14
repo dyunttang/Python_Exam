e0573e512df1b9f313704466c027a7772dbc2dff
ssar <ssar@nate.com>
2021-09-14 18:20:04 +0900

    Python Exam done with 75%

--- exam02_python/movie_api.py
+++ exam02_python/movie_api.py
@@ -1,3 +1,4 @@
+from movie_model import MovieModel
 import requests
 
 
@@ -9,4 +10,15 @@ def callMovieApi(page=1):
 
     responseDict = response.json()  # 딕셔너리 타입으로 변환
     movies = responseDict["data"]["movies"]  # list 타입
-    return movies
+    return convert_model(movies)
+
+
+def convert_model(movies):
+    list = []
+
+    for movie in movies:
+        movie_model = MovieModel(
+            movie["title"], movie["rating"], movie["small_cover_image"], movie["summary"])
+        list.append(movie_model)
+
+    return list
