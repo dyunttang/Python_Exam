dfe51d65a0a8f00bf7b7e9755aac27aaa73ad1ee
ssar <ssar@nate.com>
2021-09-14 18:48:59 +0900

    Python Exam clear

---
+++ exam02_python/weather_api.py
@@ -0,0 +1,14 @@
+from bs4.element import TemplateString
+import requests
+from bs4 import BeautifulSoup
+
+
+def getTemp():
+    url = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EB%82%A0%EC%94%A8"
+    response = requests.get(url)
+
+    naver_html = response.text
+    soup = BeautifulSoup(naver_html, 'html.parser')
+
+    temp = soup.select_one(".todaytemp")
+    return temp.text
